c2a3265c69b38c51a328c5c2cd7c6c7f720730ed
xKratos19 <turcuoctavian2005@gmail.com>
2023-08-01 16:59:58 +0300

    Finished: Backend+Frontend working

--- src/App.js
+++ src/App.js
@@ -1,55 +1,110 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [name,setName] = useState('');
+  const [datetime,setDatetime] = useState('');
+  const [description,setDescription] = useState('');
+  const [transactions, setTransactions] = useState([]);
+  
+  useEffect(() => {
+    getTransactions().then(setTransactions);
+  }, []);
+
+  async function getTransactions() {
+    // const url = (process.env.REACT_APP_API_URL + "/transaction");
+    const url = "http://localhost:4000/api" + "/transaction";
+    const response = await fetch(url);
+    return await response.json();
+  }
+
+  let balance = 0;
+  for (const transaction of transactions) {
+    balance = balance + transaction.price;
+  }
+  balance = balance.toFixed(2);
+  
+  function addNewTransaction(e) {
+    e.preventDefault();
+    // const url = (process.env.REACT_APP_API_URL + "/transaction");
+    const url = "http://localhost:4000/api" + "/transaction";
+    console.log(url);
+
+    const price = name.split(" ")[0];
+
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        price,
+        name: name.substring(price.length + 1),
+        description,
+        datetime,
+      }),
+    }).then((response) => {
+      response.json().then((json) => {
+        setName("");
+        setDatetime("");
+        setDescription("");
+        console.log("result", json);
+      });
+    });
+  }
+
   return (
     <main>
-      <h1>$400<span>.00</span></h1>
-      <form>
+      <h1>
+        {balance}
+        
+      </h1>
+      <form action="" onSubmit={addNewTransaction}>
         <div className="basic">
-          <input type="text" placeholder={'+200 nwe samsung'}/>
-          <input type="datetime-local"/>
-          </div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder={"price and name"}
+          />
+          <input
+            value={datetime}
+            onChange={(e) => setDatetime(e.target.value)}
+            type="datetime-local"
+          />
+        </div>
         <div className="description">
-        <input type="text" placeholder={'description'}/> 
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder={"description"}
+          />
         </div>
         <button type="submit">Add new transaction</button>
       </form>
       <div className="transactions">
-        <div className="transaction"></div>
-        <div className="left">
-          <div className="name">New samsung tv</div>
-          <div className="description">It was time for new tv</div>
-        </div>
-        <div className="right">
-          <div className="price red">$500</div>
-          <div className="datetime">2023-01-08</div>
-        </div>
-      </div>
-      <div className="transactions">
-        <div className="transaction"></div>
-        <div className="left">
-          <div className="name">New samsung tv</div>
-          <div className="description">It was time for new tv</div>
-        </div>
-        <div className="right">
-          <div className="price red">$500</div>
-          <div className="datetime">2023-01-08</div>
-        </div>
-      </div>
-      <div className="transactions">
-        <div className="transaction"></div>
-        <div className="left">
-          <div className="name">New samsung tv</div>
-          <div className="description">It was time for new tv</div>
-        </div>
-        <div className="right">
-          <div className="price green">$500</div>
-          <div className="datetime">2023-01-08</div>
-        </div>
+        {transactions.length > 0 &&
+          transactions.map((transaction, index) => (
+            <div key={index} className="transaction">
+              <div className="left">
+                <div className="name">{transaction.name}</div>
+                <div className="description">{transaction.description}</div>
+              </div>
+              <div className="right">
+                {/* console.log(transaction.price); */}
+                <div
+                  className={
+                    "price " + (transaction.price < 0 ? "red" : "green")
+                  }
+                >
+                  {transaction.price}
+                </div>
+                <div className="datetime">{transaction.datetime}</div>
+              </div>
+            </div>
+          ))}
       </div>
     </main>
   );
 }
-
 export default App;
